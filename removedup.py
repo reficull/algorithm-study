eaf0a133eb53e02a1a1c3ff1f562249ca9464c6b
Your Name <you@example.com>
2020-03-04 00:38:28 +0800

    correct answer

--- removedup.py
+++ removedup.py
@@ -1,18 +1,16 @@
-def removeDup(nums):
-    def rmNext(i,nums):
-        if i+1 < len(nums)-1  and nums[i] == nums[i+1]:
+
+class Solution:
+    def rmNext(self,i, nums):
+        if i+1 < len(nums) and nums[i] == nums[i+1]:
             del nums[i+1]
-            i = max(i-1,0) 
+            i = i-1
         if i+1 >= len(nums):
             return len(nums)
-        return rmNext(i+1,nums)
+        return self.rmNext(i+1, nums)
 
-    count = len(nums)
-    count = rmNext(0,nums)
+    def removeDuplicates(self, nums) -> int:
 
-    return count
+        count = len(nums)
+        count = self.rmNext(0, nums)
 
-arr = [0,0,1,1,1,2,2,3,3,4]
-count = removeDup(arr)
-print('count:',count)
-print(arr)
+        return count
